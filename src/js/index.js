721fd53d49e24e7cf57eae73836a677e618a63e1
mariab19 <clara.barth@hotmail.de>
2021-03-28 22:04:06 +0200

    remove console.log

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     var fontSize = parseInt($('#root').css('font-size'));
-    console.log("font-size: ", fontSize)
     $('button').on("click", function(){
-        console.log("this.id: ", this.id)
         if (this.id == "font-increase-button-1") {
             fontSize+=5;
         }
